049fd627dcc7acaf9ee14692298a364405764c1b
Fatma <husseinfatma523@gmail.com>
2024-03-31 06:03:45 +0200

    calculated time AC and PSD

--- Transmitter_Line_Codes.m
+++ Transmitter_Line_Codes.m
@@ -1,6 +1,6 @@
 %% Clearing workspace
-clear all;
-close all;
+%clear all;
+%close all;
 
 main();
 
@@ -73,7 +73,7 @@ function time_mean(ensemble, str, num_samples, num_realizations, ylim_start, yli
             
             Tx2 = repmat(Tx, num_samples_per_bit, 1);
             if strcmp(code_type, 'polarRZ')
-                zero_duration_start = 6;
+                zero_duration_start = 5;
                 zero_duration_end = 7;
                 Tx2(zero_duration_start:zero_duration_end,:) = 0;
             end
@@ -112,7 +112,7 @@ function time_mean(ensemble, str, num_samples, num_realizations, ylim_start, yli
         Rx = cat(2,Rx(1,1:num_samples-1),Rx(1,num_samples+1:2*num_samples));%remove Rx(0) as it repeated twice
         time = -699:699;
         subplot(2,1,1);
-        plot(time*10,Rx);
+        plot(time,Rx);
         axis([-num_samples num_samples -3 20])
         title(str);
         ylabel('magnitude');
@@ -120,33 +120,50 @@ function time_mean(ensemble, str, num_samples, num_realizations, ylim_start, yli
     end
 
 % Description:
-%   Calculate the time Autocorrelation of line code signals across different realizations
+%   Calculate the time Autocorrelation of he first realization of each line
+%   code signal.
 %   The resulting plot is titled with the provided string 'str'.
-    function time_autocorrelation(ensemble, str, num_samples, num_realizations)
+    function time_autocorrelation(ensemble, str, num_samples)
         
           % Pre-allocate the autocorrelation matrix
-        Rx = zeros(num_realizations, num_realizations);
-        
-        % Loop through each realization
-        for m=1:num_realizations
-            for n=m:num_realizations
-                Rx(m,n-m+1)=MEAN(ensemble(m,:).*ensemble(n,:),num_samples);
-                
+        Rx = zeros(1, num_samples);
+      
+           % Loop through time for the first realization
+        for tau=0:num_samples-1
+            sum=0;
+            for n=1:num_samples-tau
+                R=ensemble(1,n)*ensemble(1,n+tau);
+                sum=sum+R;
             end
+           Rx(1,tau+1)=sum/num_samples;
         end
-        Rx = [fliplr(Rx) Rx];%flip the matrix and concatenate , as Rx is even function
-        Rx = cat(2,Rx(1,1:499),Rx(1,501:1000));%remove Rx(0) as it repeated twice
-        time = -499:499;
+ Rx = [fliplr(Rx) Rx];%flip the matrix and concatenate , as Rx is even function
+ Rx = cat(2,Rx(1,1:num_samples-1),Rx(1,num_samples+1:2*num_samples));%remove Rx(0) as it repeated twice
+        time = -699:699;
         subplot(2,1,2);
-        plot(time*10,Rx);
-        axis([-num_realizations num_realizations -3 20])
+        plot(time,Rx);
+        axis([-num_samples+1 num_samples-1 -3 20])
         title(str);
         ylabel('magnitude');
         xlabel('tau');
     end
 
-
-
+% Description:
+%   This function calulates and plots the PSD of the first realization of each line
+%   code signal.
+%   The resulting plot is titled with the provided string 'str'.
+   function PSD(realization_1,str,num_samples)
+         signal_spectrum = fft(realization_1);
+         spectrum_L = length(signal_spectrum);
+         %% Adjust the Axis scale so the signal spectrum is
+         %% plotted versus frequency centered at zero.
+         k = -spectrum_L/2:spectrum_L/2-1;    
+         figure;
+         plot (k,fftshift(abs(signal_spectrum)));
+         title(str);
+         xlabel('Frequency (Hz)');
+         ylabel('Magnitude');
+    end      
 % Description:
 %   Entry point for executing the main functionality of the MATLAB script.
 %   This function contains the main code logic that will be executed.
@@ -170,7 +187,8 @@ function time_mean(ensemble, str, num_samples, num_realizations, ylim_start, yli
         
         %%%% Generation of polar NRZ line code %%%
         ensemble = generate_line_code(Data, "polarNRZ", A, ensemble, num_realizations);
-        
+        %%%% calculate and plot the PSD of the first realization of polar NRZ line code %%%
+        PSD(ensemble(1,:),("PSD of the first realization of polar NRZ line code"),num_samples);
         %%%% plot 4 realizations of polar NRZ line code %%%
         plot_line_codes(ensemble, ('polar NRZ line code realisation '), ylim_start, ylim_end);
         
@@ -180,9 +198,14 @@ function time_mean(ensemble, str, num_samples, num_realizations, ylim_start, yli
         
         %%%% calculate the statistical and time Autocorrelation of polar NRZ
         statistical_autocorrelation(ensemble, ("statistical Autocorrelation - polar NRZ line code"), num_samples, num_realizations)
-        time_autocorrelation(ensemble, ("time Autocorrelation - polar NRZ line code"), num_samples, num_realizations);
+        time_autocorrelation(ensemble, ("time Autocorrelation - polar NRZ line code"), num_samples);
+       
+        
         %%%% Generation of Unipolar line code %%%
         ensemble = generate_line_code(Data, "unipolar", A, ensemble, num_realizations);
+        
+        %%%% calculate and plot the PSD of the first realization of polar NRZ line code %%%
+        PSD(ensemble(1,:),("PSD of the first realization of unipolar line code"),num_samples);
         
         %%%% plot 4 realizations of Unipolar line code %%%
         plot_line_codes(ensemble, ('Unipolar line code realisation '), ylim_start, ylim_end);
@@ -192,10 +215,14 @@ function time_mean(ensemble, str, num_samples, num_realizations, ylim_start, yli
         time_mean(ensemble, ("time mean - Unipolar line code"), num_samples, num_realizations, ylim_start, ylim_end);
         %%%% calculate the statistical and time Autocorrelation of Unipolar
         statistical_autocorrelation(ensemble, ("statistical Autocorrelation - Unipolar line code"), num_samples, num_realizations)
-        time_autocorrelation(ensemble, ("time Autocorrelation - Unipolar line code"), num_samples, num_realizations);
+        time_autocorrelation(ensemble, ("time Autocorrelation - Unipolar line code"), num_samples);
+      
         %%%% Generation of polar RZ line code %%%
         ensemble = generate_line_code(Data, "polarRZ", A, ensemble, num_realizations);
         
+        %%%% calculate and plot the PSD of the first realization of polar NRZ line code %%%
+        PSD(ensemble(1,:),("PSD of the first realization of polarRZ line code "),num_samples);
+       
         %%%% plot 4 realizations of polar RZ line code %%%
         plot_line_codes(ensemble, ('polar RZ line code realisation '), ylim_start, ylim_end);
         
@@ -205,6 +232,6 @@ function time_mean(ensemble, str, num_samples, num_realizations, ylim_start, yli
         
          %%%% calculate the statistical and time Autocorrelation of polar RZ
         statistical_autocorrelation(ensemble, ("statistical Autocorrelation - polar RZ line code"), num_samples, num_realizations)
-        time_autocorrelation(ensemble, ("time Autocorrelation - polar RZ line code"), num_samples, num_realizations);
+        time_autocorrelation(ensemble, ("time Autocorrelation - polar RZ line code"), num_samples);
         
     end
